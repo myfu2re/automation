0ebb97d09dc4ab2cb948d1d1b8854ff19f7e83b0
Pavlo Smolianinov <pavlo.smolianinov@medtronic.com>
2024-11-11 18:19:29 +0200

    add backup.sh

---
+++ bash_examples/backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Налаштування
+SOURCE_DIR="/path/to/source"
+BACKUP_DIR="/path/to/backup"
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="backup_$DATE.tar.gz"
+
+# Створення резервної копії
+echo "Creating a backup copy $SOURCE_DIR..."
+tar -czf "$BACKUP_DIR/$BACKUP_FILE" "$SOURCE_DIR"
+
+if [ $? -eq 0 ]; then
+    echo "Backup completed successfully! File saved as $BACKUP_DIR/$BACKUP_FILE"
+else
+    echo "An error occurred while creating a backup copy."
+fi
